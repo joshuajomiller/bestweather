6130dc2f61b6a03d20c80d9b7c09d7d893ee4164
joshuajomiller <joshuajosephmiller@gmail.com>
2018-03-28 00:42:11 +0300

    video cover component

---
+++ js/video-cover/video-cover.component.js
@@ -0,0 +1,54 @@
+angular.module('weatherApp').component('videoCoverComponent', {
+  templateUrl: 'js/video-cover/video-cover.template.html',
+  controller: videoCoverController
+});
+function videoCoverController(){
+  let self = this;
+
+  self.$onInit = function(){
+    $( document ).ready(function() {
+      scaleVideoContainer();
+      initBannerVideoSize('.video-container .poster img');
+      initBannerVideoSize('.video-container .filter');
+      initBannerVideoSize('.video-container video');
+      $(window).on('resize', function() {
+        scaleVideoContainer();
+        scaleBannerVideoSize('.video-container .poster img');
+        scaleBannerVideoSize('.video-container .filter');
+        scaleBannerVideoSize('.video-container video');
+        });
+    });
+  };
+  
+  function scaleVideoContainer() {
+    let height = $(window).height();
+    let unitHeight = parseInt(height) + 'px';
+    $('.homepage-hero-module').css('height',unitHeight);
+  }
+
+  function initBannerVideoSize(element){
+    $(element).each(function(){
+      $(this).data('height', $(this).height());
+      $(this).data('width', $(this).width());
+    });
+    scaleBannerVideoSize(element);
+  }
+
+  function scaleBannerVideoSize(element){
+    let windowWidth = $(window).width(),
+      windowHeight = $(window).height() + 5,
+      videoWidth,
+      videoHeight;
+    $(element).each(function(){
+      let videoAspectRatio = $(this).data('height')/$(this).data('width');
+      $(this).width(windowWidth);
+      if(windowWidth < 1000){
+        videoHeight = windowHeight;
+        videoWidth = videoHeight / videoAspectRatio;
+        $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+        $(this).width(videoWidth).height(videoHeight);
+      }
+      $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+    });
+  }
+}
